8512be9d039ae2e87e4fa27b76d7500eb508b5cc
real0x0a1 <143189904+real0x0a1@users.noreply.github.com>
2024-05-09 10:35:02 +0330

    Updated heart_disease_prediction.py

--- heart_disease_prediction.py
+++ heart_disease_prediction.py
@@ -63,7 +63,7 @@ input_data = (63,   # age
 # convert the input data to a numpy array
 input_data_as_numpy_array= np.asarray(input_data)
 
-# reshape the numpy array as we are predicting for only on instance
+# reshape the numpy array as we are predicting for only one instance
 input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
 prediction = model.predict(input_data_reshaped)
